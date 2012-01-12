404676ddb8d284b18b1698a546fee253b5e279f4
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2012-01-12 15:39:12 +0200

    Introduce git-buildpackage-rpm tool
    Initial version of git-buildpackage-rpm: git-buildpackage tool for rpms.
    
    Also adds some new options to config.py that are exclusively used in
    this tool.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>
    Signed-off-by: Ed Bartosh <eduard.bartosh@intel.com>
    Signed-off-by: Zhang Qiang <qiang.z.zhang@intel.com>

--- gbp/config.py
+++ gbp/config.py
@@ -97,7 +97,6 @@ class GbpOptionParser(OptionParser):
                  'snapshot-number' : 'snapshot + 1',
                  'git-log'         : '--no-merges',
                  'export'          : 'HEAD',
-                 'export-dir'      : '',
                  'overlay'         : 'False',
                  'tarball-dir'     : '',
                  'ignore-new'      : 'False',
@@ -519,6 +518,7 @@ class GbpOptionParserDebian(GbpOptionParser):
     defaults.update( {
                        'builder'            : 'debuild -i -I',
                        'cleaner'            : 'debuild -d clean',
+                       'export-dir'         : '',
                      } )
 
 class GbpOptionParserRpm(GbpOptionParser):
@@ -527,12 +527,20 @@ class GbpOptionParserRpm(GbpOptionParser):
     """
     defaults = dict(GbpOptionParser.defaults)
     defaults.update( {
-                       'builder'                : '',
+                       'builder'                : 'rpmbuild',
                        'cleaner'                : '',
                        'packaging-dir'          : '',
                        'packaging-tag'          : 'packaging/%(version)s',
                        'pq-branch'              : 'development/%(branch)s',
                        'spec-file'              : 'auto',
+                       'export-dir'             : 'rpmbuild',
+                       'rpmbuild-builddir'      : 'BUILD',
+                       'rpmbuild-rpmdir'        : 'RPMS',
+                       'rpmbuild-sourcedir'     : 'SOURCES',
+                       'rpmbuild-specdir'       : 'SPECS',
+                       'rpmbuild-srpmdir'       : 'SRPMS',
+                       'rpmbuild-buildrootdir'  : 'BUILDROOT',
+                       'patch-export'           : 'False',
                      } )
 
     help = dict(GbpOptionParser.help)
@@ -545,6 +553,8 @@ class GbpOptionParserRpm(GbpOptionParser):
                         "format string for the patch-queue branch name, default is '%(pq-branch)s'",
                    'spec-file':
                         "Spec file to use, 'auto' makes gbp to guess, other values make the packaging-dir option to be ignored, default is '%(spec-file)s'",
+                   'patch-export':
+                        "Create patches between upstream and export-treeish, default is '%(patch-export)s'",
                  } )
 
 # vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
