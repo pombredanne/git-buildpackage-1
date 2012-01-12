979924579da36030b927c89b57ad2be172825d7e
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2012-01-12 15:35:20 +0200

    Rename internal var debian_branch -> packaging_branch
    This is done in sake of more general purpose naming, intended for
    enabling rpm support.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/dch.py
+++ gbp/scripts/dch.py
@@ -357,7 +357,7 @@ def main(argv):
     parser.add_option_group(custom_group)
 
     parser.add_boolean_config_file_option(option_name = "ignore-branch", dest="ignore_branch")
-    naming_group.add_config_file_option(option_name="debian-branch", dest="debian_branch")
+    naming_group.add_config_file_option(option_name="debian-branch", dest="packaging_branch")
     naming_group.add_config_file_option(option_name="upstream-tag", dest="upstream_tag")
     naming_group.add_config_file_option(option_name="debian-tag", dest="debian_tag")
     naming_group.add_config_file_option(option_name="snapshot-number", dest="snapshot_number",
@@ -421,8 +421,8 @@ def main(argv):
             raise GbpError("%s is not a git repository" % (os.path.abspath('.')))
 
         branch = repo.get_branch()
-        if options.debian_branch != branch and not options.ignore_branch:
-            gbp.log.err("You are not on branch '%s' but on '%s'" % (options.debian_branch, branch))
+        if options.packaging_branch != branch and not options.ignore_branch:
+            gbp.log.err("You are not on branch '%s' but on '%s'" % (options.packaging_branch, branch))
             raise GbpError("Use --ignore-branch to ignore or --debian-branch to set the branch name.")
 
         cp = ChangeLog(filename=changelog)
