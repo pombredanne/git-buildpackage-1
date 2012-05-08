agx/git-buildpackage

commit f3aa87fa0361acf902e8c95eefab6dd6b66526fa

GitRepository.merge: add edit option
defaulting to False

Closes: #659239

gbp/git/repository.py
@@ -299,7 +299,7 @@ class GitRepository(object):
         remote += merge.replace("refs/heads","", 1)
         return remote
 
-    def merge(self, commit, verbose=False):
+    def merge(self, commit, verbose=False, edit=False):
         """
         Merge changes from the named commit into the current branch
 
@@ -307,9 +307,12 @@ class GitRepository(object):
         @type commit: C{str}
         @param verbose: whether to print a summary after the merge
         @type verbose: C{bool}
+        @param edit: wheter to invoke an editor to edit the merge message
+        @type edit: C{bool}
         """
         args = GitArgs()
         args.add_cond(verbose, '--summary', '--no-summary')
+        args.add_cond(edit, '--edit', '--no-edit')
         args.add(commit)
         self._git_command("merge", args.args)
 

pombredanne/git-buildpackage-1

commit c32ef4e01f8dbcd19119b0fdb57d48e309cb346a

GitRepository: fix merge() for older git versions
Adds a new method for getting the version of git suite (i.e. git utils)
installed on the OS running gbp.

Utilize the new function in GitRepository.merge() to not give
edit/no-edit option for older versions of git-merge, that don't support
it. Fixes a regression (with git-version < 1.7.8) caused by commit
f3aa87fa0361a.

gbp/git/repository.py
@@ -157,6 +157,26 @@ class GitRepository(object):
         """
         GitCommand(command, args, extra_env=extra_env, cwd=self.path)()
 
+    def _get_git_version(self):
+        """
+        Return the version of the git suite (i.e. "git client commands")
+        that is installed on the OS.
+
+        The version is not a property of the git repository itself, but the
+        information it is needed in some methods of GitRepository() in order
+        to call git subcommands with correct arguments.
+
+        @return: git version
+        @rtype: C{str}
+        """
+        out = self._git_inout("version", [])
+        # effectively "lstrip" everything until the first number
+        version_re = re.compile(r'^[^0-9]*(?P<version>.*)$')
+        m = version_re.match(out[0])
+        if m:
+            return m.group('version')
+        return None
+
     @property
     def path(self):
         """The absolute path to the repository"""
@@ -312,7 +332,10 @@ class GitRepository(object):
         """
         args = GitArgs()
         args.add_cond(verbose, '--summary', '--no-summary')
-        args.add_cond(edit, '--edit', '--no-edit')
+        if (self._get_git_version() >= '1.7.8'):
+            args.add_cond(edit, '--edit', '--no-edit')
+        else:
+            log.debug("edit/no-edit option for git-merge not supported by your git suite version")
         args.add(commit)
         self._git_command("merge", args.args)
 