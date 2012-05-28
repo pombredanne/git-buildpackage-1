59c6698a4a0c712069ff4c833ed85408a564aab5
Ed Bartosh <eduard.bartosh@intel.com>
2012-05-28 14:22:21 +0300

    buildpackage_rpm: Changed default git-archive prefix to %{name}-%{version}
    Change-Id: I4f306e11204e1b2d8d909471273469fc5374d181

--- gbp/scripts/buildpackage_rpm.py
+++ gbp/scripts/buildpackage_rpm.py
@@ -50,7 +50,7 @@ def git_archive(repo, spec, output_dir, treeish, comp_type, comp_level, with_sub
             raise GbpError, "Unsupported compression type '%s'" % comp_type
 
     output = os.path.join(output_dir, os.path.basename(spec.orig_file))
-    prefix = spec.orig_base
+    prefix = "%s-%s" % (spec.name, spec.version)
 
     try:
         if repo.has_submodules() and with_submodules:
