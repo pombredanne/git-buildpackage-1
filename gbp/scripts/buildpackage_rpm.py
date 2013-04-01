2d2c204fed2a40fba23fe8379ac49357e3904cc1
Zhang Qiang <qiang.z.zhang@intel.com>
2013-04-01 16:49:54 +0800

    don't re-create tar ball if already exist.
    without this patch, tall ball always be re-created even if already
    exist for native package.
    
    Change-Id: Ie33569c3da77013bf2895c0f5f9b0d9752cf906f

--- gbp/scripts/buildpackage_rpm.py
+++ gbp/scripts/buildpackage_rpm.py
@@ -542,7 +542,8 @@ def main(argv):
 
             # Get/build the orig tarball
             if is_native(repo, options):
-                if spec.orig_src:
+                orig_file = spec.orig_src['filename']
+                if spec.orig_src and not RpmPkgPolicy.has_orig(orig_file, source_dir):
                     # Just build source archive from the exported tree
                     gbp.log.info("Creating (native) source archive %s from '%s'" % (spec.orig_src['filename'], tree))
                     if spec.orig_src['compression']:
