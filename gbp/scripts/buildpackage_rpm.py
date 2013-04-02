c816778457fb2a55a6fc2747f29b2aa31d0b7a96
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-04-02 10:30:58 +0300

    buildpackage-rpm: Revert tarball creation HACKs
    Revert commits f04d3c96e3612855e23c87d60a2614ac6ee71a50 and
    2d2c204fed2a40fba23fe8379ac49357e3904cc1.
    
    We cannot do this for native packages for which the orig source tarball
    changes after every change.

--- gbp/scripts/buildpackage_rpm.py
+++ gbp/scripts/buildpackage_rpm.py
@@ -542,7 +542,7 @@ def main(argv):
 
             # Get/build the orig tarball
             if is_native(repo, options):
-                if spec.orig_src and not RpmPkgPolicy.has_orig(spec.orig_src['filename'], source_dir):
+                if spec.orig_src:
                     # Just build source archive from the exported tree
                     gbp.log.info("Creating (native) source archive %s from '%s'" % (spec.orig_src['filename'], tree))
                     if spec.orig_src['compression']:
