f3fb7923973faab0c3bb1349517164da47d17784
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-04-09 16:48:45 +0300

    pq-rpm: don't create pq branch when switching
    Pq branch must be created with 'import'.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/component/rpm/test_pq_rpm.py
+++ tests/component/rpm/test_pq_rpm.py
@@ -160,18 +160,22 @@ class TestPqRpm(RpmRepoTestBase):
         repo = self.init_test_repo('gbp-test')
         pkg_files = repo.list_files()
         branches = repo.get_local_branches() + ['development/master']
-        upstr_files = ['dummy.sh', 'Makefile', 'README']
-        # Switch to non-existent pq-branch should create one
+        # Switch to non-existent pq-branch should fail
+        eq_(mock_pq(['switch']), 1)
+        self._check_log(-1, ".*Branch 'development/master' does not exist")
+
+        # Import and switch to base branch and back to pq
+        eq_(mock_pq(['import']), 0)
         eq_(mock_pq(['switch']), 0)
-        self._check_repo_state(repo, 'development/master', branches,
-                               upstr_files)
+        self._check_repo_state(repo, 'master', branches)
+        eq_(mock_pq(['switch']), 0)
+        self._check_repo_state(repo, 'development/master', branches)
 
         # Switch to base branch and back to pq
         eq_(mock_pq(['switch']), 0)
         self._check_repo_state(repo, 'master', branches, pkg_files)
         eq_(mock_pq(['switch']), 0)
-        self._check_repo_state(repo, 'development/master', branches,
-                               upstr_files)
+        self._check_repo_state(repo, 'development/master', branches)
 
     def test_switch_drop(self):
         """Basic test for drop action"""
