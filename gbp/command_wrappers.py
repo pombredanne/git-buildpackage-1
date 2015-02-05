e51b99945ca8ffef37b65346a3f8ecfa72a55f66
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2015-02-05 17:13:11 +0200

    Command: redirect stderr to stdout
    Makes it possible to get all command output from stdout.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/command_wrappers.py
+++ gbp/command_wrappers.py
@@ -63,7 +63,8 @@ class Command(object):
 
         log.debug("%s %s %s" % (self.cmd, self.args, args))
         self.stderr = ''
-        stderr_arg = subprocess.PIPE if self.capture_stderr else None
+        stderr_arg = subprocess.PIPE if self.capture_stderr else \
+                                        subprocess.STDOUT
         cmd = [ self.cmd ] + self.args + args
         if self.shell:
             # subprocess.call only cares about the first argument if shell=True
